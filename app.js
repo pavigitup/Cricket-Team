088e715152591a9466897c2c010e7c0897b9b61a
Pavithra G <pavithragokali@gmail.com>
2023-11-19 10:45:15 +0000

    finished: git commit -m finished:

--- app.js
+++ app.js
@@ -51,13 +51,63 @@ app.post("/players/", async (request, response) => {
   const playersList = request.body;
   const { playerName, jerseyNumber, role } = playersList;
   const createNewPlayer = `
-    INSERT INTO cricket_team (player_name, jersey_number, role)
-    VALUES (${playerName},${jerseyNumber},${role});`;
+   INSERT INTO
+      cricket_team (player_name, jersey_number,role)
+    VALUES
+      (
+        '${playerName}',
+         ${jerseyNumber},
+         '${role}'
+         
+      );`;
   const dbResponse = await db.run(createNewPlayer);
-  console.log(dbResponse);
-  const playerId = dbResponse.lastID;
-  console.log(playerId);
-  response.send({ playerId: playerId });
+  response.send("Player Added to Team");
+});
+
+//Returns a player based on a player ID
+app.get("/players/:playerId/", async (request, response) => {
+  const playerId = request.params.playerId;
+
+  const getPlayer = `
+    SELECT *
+    FROM cricket_team
+    WHERE player_id = ${playerId};`;
+  const player = await db.get(getPlayer);
+  const ansPlayer = (player) => {
+    return {
+      playerId: player.player_id,
+      playerName: player.player_name,
+      jerseyNumber: player.jersey_number,
+      role: player.role,
+    };
+  };
+  response.send(ansPlayer(player));
+});
+
+//Updates the details of a player in the team (database) based on the player ID
+app.put("/players/:playerId/", async (request, response) => {
+  const player1Id = request.params.playerId;
+  const updateDetails = request.body;
+  const { playerName, jerseyNumber, role } = updateDetails;
+  const updateTables = `
+    UPDATE cricket_team
+    SET 
+      player_name= '${playerName}',
+      jersey_number= ${jerseyNumber},
+      role= '${role}'
+    WHERE player_id = ${player1Id};`;
+  await db.run(updateTables);
+  response.send("Player Details Updated");
+});
+
+//Deletes a player from the team (database) based on the player ID
+app.delete("/players/:playerId/", async (request, response) => {
+  const playerId = request.params.playerId;
+  const deletePlayer = `
+    DELETE FROM cricket_team
+    WHERE player_id = ${playerId};`;
+  await db.run(deletePlayer);
+  response.send("Player Removed");
 });
 
 module.exports = app;
